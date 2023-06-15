35279e0574b3bebd7191fea75329e20cf84ec481
pustart <polaris9955@gmail.com>
2023-06-15 11:36:30 +0300

    refactor: update linting configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'nestjs'
   ],
   root: true,
   env: {
@@ -21,5 +22,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    indent: [
+      'warn',
+      2
+    ],
+    semi: 'error'
   },
 };
