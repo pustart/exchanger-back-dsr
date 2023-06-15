326e5ad442beb57eda2db2160451eaace7f61181
pustart <polaris9955@gmail.com>
2023-06-15 11:26:23 +0300

    feat: init swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: process.env.CLIENT,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle("MoneyPie API")
+    .setDescription("Exchanger app REST API documentation")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
