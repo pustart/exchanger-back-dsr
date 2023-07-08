09db251aba8da4bcba09a7a796aea45162273bce
pustart <polaris9955@gmail.com>
2023-07-08 17:20:05 +0300

    feat: add auth module

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,6 @@
+export const ALREADY_REGISTERED_ERROR = "Такой пользователь уже был зарегистрирован.";
+export const USER_NOT_FOUND_ERROR = "Пользователь с таким e-mail не найден.";
+export const WRONG_PASSWORD_ERROR = "Неверный пароль.";
+export const USER_NOT_AUTHORIZED = "Пользователь не авторизован.";
+export const LOGIN_ERROR = "Ошибка при попытке логина, попробуйте позже";
+export const REGISTRATION_ERROR = "Ошибка при попытке регистрации, попробуйте позже";
