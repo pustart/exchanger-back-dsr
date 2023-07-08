0ad7f73d672f75079ad8dc1e7f01a8ca11d90cc7
pustart <polaris9955@gmail.com>
2023-07-09 00:25:57 +0300

    feat: add things counter in json response

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
+import { CATEGORY_FOUND_ERROR } from "./categories.constants";
 import { CreateCategoryDto } from "./dto/create-category.dto";
 import { UpdateCategoryDto } from "./dto/update-category.dto";
 
@@ -11,12 +12,35 @@ export class CategoriesService {
     return this.prisma.category.create({ data: createCategoryDto });
   }
 
-  findAll() {
-    return this.prisma.category.findMany();
+  async findAll() {
+    const categories = await this.prisma.category.findMany({
+      include: {
+        thing: true,
+      },
+    });
+
+    const categoriesWithCount = categories.map((category) => {
+      const thingsCount = category.thing.length;
+      return { id: category.id, name: category.name, thingsCount };
+    });
+
+    return categoriesWithCount;
   }
 
-  findOne(categoryId: string) {
-    return this.prisma.category.findUnique({ where: { id: categoryId } });
+  async findOne(categoryId: string) {
+    const category = await this.prisma.category.findUnique({
+      where: { id: categoryId },
+      include: {
+        thing: true,
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException(CATEGORY_FOUND_ERROR);
+    }
+
+    const thingsCount = category.thing.length;
+    return { id: category.id, name: category.name, thingsCount };
   }
 
   update(categoryId: string, updateCategoryDto: UpdateCategoryDto) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/database/prisma.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
@@ -30,14 +30,53 @@ export class UsersService {
     });
   }
 
-  findAll() {
-    return this.prisma.user.findMany();
+  async findAll() {
+    const users = await this.prisma.user.findMany({
+      include: {
+        things: true,
+      },
+    });
+
+    const usersWithCount = users.map((user) => {
+      const thingsCount = user.things.length;
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        birthday: user.birthday,
+        photo: user.photo,
+        role: user.role,
+        thingsCount,
+      };
+    });
+
+    return usersWithCount;
   }
 
-  findOne(id: string) {
-    return this.prisma.user.findUnique({
+  async findOne(id: string) {
+    const user = await this.prisma.user.findUnique({
       where: { id },
+      include: {
+        things: true,
+      },
     });
+
+    if (!user) {
+      throw new NotFoundException(FIND_USER_ERROR);
+    }
+
+    const thingsCount = user.things.length;
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      birthday: user.birthday,
+      photo: user.photo,
+      role: user.role,
+      thingsCount,
+    };
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
