12b55bba0c65c77f674d8838516b361e54c30c53
pustart <polaris9955@gmail.com>
2023-07-10 06:08:15 +0300

    feat: add file interceptors for photos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { ErrorResponse } from "src/exceptions/response/exceptions.response";
 import { CreateUserDto } from "../users/dto/create-user.dto";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/auth.dto";
 import { AuthUserResponse } from "./response/auth.responses";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { v4 as uuidv4 } from "uuid";
 
 @ApiTags("Log in and sign up endpoints")
 @Controller("auth")
@@ -39,7 +50,21 @@ export class AuthController {
   @ApiResponse({ status: 500, type: ErrorResponse })
   @Post("/registration")
   @HttpCode(HttpStatus.OK)
-  registration(@Body() userDto: CreateUserDto) {
+  @UseInterceptors(
+    FileInterceptor("photo", {
+      storage: diskStorage({
+        destination: "./public",
+        filename: (req, file, cb) => {
+          const filename = uuidv4();
+          const extension = file.originalname.split(".").pop();
+          cb(null, `${filename}.${extension}`);
+        },
+      }),
+    }),
+  )
+  registration(@UploadedFile() photo: Express.Multer.File, @Body() userDto: CreateUserDto) {
+    const photoUrl = photo ? `/${photo.filename}` : null;
+    userDto.photo = photoUrl;
     return this.authService.registration(userDto);
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+} from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/auth/guards/jwt.guard";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { v4 as uuidv4 } from "uuid";
 
 @ApiTags("Users")
 @Controller("users")
@@ -13,7 +27,21 @@ export class UsersController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
-  create(@Body() createUserDto: CreateUserDto) {
+  @UseInterceptors(
+    FileInterceptor("photo", {
+      storage: diskStorage({
+        destination: "./public",
+        filename: (req, file, cb) => {
+          const filename = uuidv4();
+          const extension = file.originalname.split(".").pop();
+          cb(null, `${filename}.${extension}`);
+        },
+      }),
+    }),
+  )
+  create(@UploadedFile() photo: Express.Multer.File, @Body() createUserDto: CreateUserDto) {
+    const photoUrl = photo ? `/${photo.filename}` : null;
+    createUserDto.photo = photoUrl;
     return this.usersService.create(createUserDto);
   }
 
@@ -31,7 +59,25 @@ export class UsersController {
 
   @Patch(":id")
   @UseGuards(JwtAuthGuard)
-  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+  @UseInterceptors(
+    FileInterceptor("photo", {
+      storage: diskStorage({
+        destination: "./public",
+        filename: (req, file, cb) => {
+          const filename = uuidv4();
+          const extension = file.originalname.split(".").pop();
+          cb(null, `${filename}.${extension}`);
+        },
+      }),
+    }),
+  )
+  update(
+    @UploadedFile() photo: Express.Multer.File,
+    @Param("id") id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    const photoUrl = photo ? `/${photo.filename}` : null;
+    updateUserDto.photo = photoUrl;
     return this.usersService.update(id, updateUserDto);
   }
 
